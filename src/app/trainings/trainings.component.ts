2fc287d9766b5b5c55b8d314d2148eee40652c16
Kumar Sidharth <sid@Kumars-MacBook-Pro.local>
2020-08-21 17:18:33 +0530

    filtered trainings

--- src/app/trainings/trainings.component.ts
+++ src/app/trainings/trainings.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { TrainingService } from '../services/training.service';
-import { Observable, of } from 'rxjs';
+import { of, forkJoin } from 'rxjs';
 import { Training } from '../models/training';
-import { take, catchError } from 'rxjs/operators';
+import { catchError, flatMap } from 'rxjs/operators';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'infrrd-trainings',
@@ -11,20 +12,51 @@ import { take, catchError } from 'rxjs/operators';
 })
 export class TrainingsComponent implements OnInit {
 
-  trainings$: Observable<Training[]>;
+  trainings: Training[] = [];
+  filteredTrainings: Training[] = [];
   searchInput: string;
 
   constructor(
-    private trainingService: TrainingService
+    private trainingService: TrainingService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
-    this.trainings$ = this.trainingService
-                        .getTainings()
-                        .pipe(
-                          take(1),
-                          catchError( () => of([]))
-                        );
+    this.trainingService
+      .getTainings()
+      .pipe(
+        catchError( () => of([]))
+      ).subscribe( (trainings) => {
+        this.trainings = trainings;
+        this.filterTrainings();
+      });
+
+    this.activatedRoute.queryParamMap.subscribe(queryParamMap => {
+      this.searchInput = queryParamMap.get('search');
+      this.filterTrainings();
+    });
+  }
+
+  addQueryParams(event: string): void {
+    this.router.navigate(['trainings'], {
+      queryParams: {
+        search: event
+      }
+    });
+  }
+
+  filterTrainings(): void {
+    if (!this.searchInput || !Array.isArray(this.trainings)) {
+      this.filteredTrainings = this.trainings;
+      return;
+    }
+    this.filteredTrainings = this.trainings
+                        .filter(training => Object.values(training)
+                          .some(value => {
+                            const stringed = value.toString().toLowerCase();
+                            return stringed.includes(this.searchInput.toLowerCase());
+                        }));
   }
 
 }
